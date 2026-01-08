1f3d33a497d2a5f3ec0293d93138e9afc18b65ea
PoRi <1960825664@qq.com>
2026-01-08 11:05:51 +0800

    ``` refactor(contract): 移除未使用的模块导出并简化索引文件
    移除了契约层中未使用的模块导出，包括auth、company、content、inquiry、
    master-category、media、product、quotation、site、system等模块的导出。
    同时移除了未使用的tsup配置文件，保持契约层的简洁性。
    
    BREAKING CHANGE: 移除了多个模块的导出，可能影响依赖这些导出的代码
    ```

--- apps/web/server/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./_custom/ads.controller";
-export * from "./_custom/herocards.controller";
-export * from "./_custom/inquiry.controller";
-export * from "./_custom/media.controller";
-export * from "./_custom/products.controller";
-export * from "./_custom/sitecategories.controller";
-
